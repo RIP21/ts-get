1d138a70c1f96c51193d81a97a1b3881c2e25575
Andrey Los <puha212@gmail.com>
2019-03-12 23:31:52 +0100

    - Initial commit and v1.0.0 🎉🎉🎉

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # ts-get
-Alternative to lodash.get that makes it typed and cool as if optional typing proposal is there
+
+Alternative to `lodash.get` that makes it typed and cool as if optional chaining proposal is there.
+
+## Usage and features
+
+```typescript
+import get from 'ts-get'
+
+ type SomeType = {
+  optionalField?: string
+  nested?: {
+    dangerousAccess?: string
+  }
+ } | undefined | null
+ 
+ const input = {}
+ const input2 = {
+    optionalField: "value",
+ }
+ 
+ get(input, it => it.optionalField, "default") // -> "default"
+ get(input2, it => it.optionalField, "default") // -> "value"
+ get(input2, it => it.nested.dangerousAccess, "default") // -> "default"
+ get(input2, it => it.unknownField, "default") // -> Type error, `unknownField` doesn't exist on type
+ get(input2, it => it.optionalField, 5) // -> Type error, third argument is not assignable to type `string`
+```
+
+## Difference with `lodash.get` behavior
+
+- If your path gets `null` at the end, it will bail out to `defaultValue` or `undefined`. 
+If you would like to get `null` returned anyway, just pass it as a `defaultValue`
+- If your type field is of type `null` and only `null` or `undefined` your field will be of type `{}[]`. 
+I have no idea how to fix it 🤷‍♂️ PR Welcome 😇🙏
+```typescript
+type A = {
+  field: null | undefined// -> {}[] inside of the callback and as return type too
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+/** Removes all undefined and nulls from the nested types making all nested fields
+ * non-nullable hence you can simply chain through it and autosuggestion will work
+ * and will not complain on undefiness and nullability of some of the nested fields
+ * including the root
+ *
+ * @example
+ * type AllOpt = {
+ *  a?: string
+ *  arr?: string[] | null
+ *  b?: {
+ *    c?: string | undefined | null
+ *    n?:
+ *      | {
+ *          a?: string | null
+ *          b: object | null
+ *        }
+ *      | null
+ *      | undefined
+ *  }
+ *  c?: string
+ * }
+ *
+ * type AllReq = RequireRecursively<AllOpt | undefined | null>
+ *
+ * const obj: AllReq = ({} as unknown) as AllReq
+ *
+ *  obj.b.c // -> string
+ *  obj.b.n.a // -> string
+ *  obj.b.n.b // -> object
+ *  obj.arr // -> string[]
+ *
+ *
+ */
+export type RequiredRecursively<T> = Exclude<
+    {
+        [P in keyof T]-?: T[P] extends (infer U)[]
+        ? RequiredRecursively<U>[]
+        : RequiredRecursively<T[P]>
+    },
+    null | undefined
+    >
+
+export type AccessorFunction<T, R> = (object: RequiredRecursively<T>) => R
+
+export function get<T, R>(
+    object: T,
+    accessorFn: AccessorFunction<T, R>,
+): R | undefined
+export function get<T, R>(
+    object: T,
+    accessorFn: AccessorFunction<T, R>,
+    defaultValue: R,
+): R
+export function get<T, R>(
+    object: T,
+    accessorFn: AccessorFunction<T, R>,
+    defaultValue?: R,
+): R | undefined {
+    try {
+        const result = accessorFn((object as unknown) as RequiredRecursively<T>)
+        return result === undefined || result === null ? defaultValue : result
+    } catch (e) {
+        return defaultValue === undefined ? undefined : defaultValue
+    }
+}
+
+export default get
