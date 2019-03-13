217c5f65fc4b34cbe4e37a4ce497796b35452809
Andrey Los <puha212@gmail.com>
2019-03-13 21:19:21 +0100

    Prettier 🚧

--- src/index.ts
+++ src/index.ts
@@ -32,36 +32,33 @@
  *
  */
 export type RequiredRecursively<T> = Exclude<
-    {
-        [P in keyof T]-?: T[P] extends (infer U)[]
-        ? RequiredRecursively<U>[]
-        : RequiredRecursively<T[P]>
-    },
-    null | undefined
-    >
+  {
+    [P in keyof T]-?: T[P] extends (infer U)[]
+      ? RequiredRecursively<U>[]
+      : RequiredRecursively<T[P]>
+  },
+  null | undefined
+>
 
 export type AccessorFunction<T, R> = (object: RequiredRecursively<T>) => R
 
+export function get<T, R>(object: T, accessorFn: AccessorFunction<T, R>): R | undefined
 export function get<T, R>(
-    object: T,
-    accessorFn: AccessorFunction<T, R>,
-): R | undefined
-export function get<T, R>(
-    object: T,
-    accessorFn: AccessorFunction<T, R>,
-    defaultValue: R,
+  object: T,
+  accessorFn: AccessorFunction<T, R>,
+  defaultValue: R,
 ): R
 export function get<T, R>(
-    object: T,
-    accessorFn: AccessorFunction<T, R>,
-    defaultValue?: R,
+  object: T,
+  accessorFn: AccessorFunction<T, R>,
+  defaultValue?: R,
 ): R | undefined {
-    try {
-        const result = accessorFn((object as unknown) as RequiredRecursively<T>)
-        return result === undefined || result === null ? defaultValue : result
-    } catch (e) {
-        return defaultValue
-    }
+  try {
+    const result = accessorFn((object as unknown) as RequiredRecursively<T>)
+    return result === undefined || result === null ? defaultValue : result
+  } catch (e) {
+    return defaultValue
+  }
 }
 
 export default get
