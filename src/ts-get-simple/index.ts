4cc0844e8ed73f443592b33aabfbe397390d510f
Andrey Los <puha212@gmail.com>
2019-03-31 01:37:53 +0100

    - Simple ts-get added for better typings in corner cases

---
+++ src/ts-get-simple/index.ts
@@ -0,0 +1,22 @@
+export type AccessorFunction<T, R> = (object: T) => R
+
+export function get<T, R>(object: T, accessorFn: AccessorFunction<T, R>): R | undefined
+export function get<T, R>(
+  object: T,
+  accessorFn: AccessorFunction<T, R>,
+  defaultValue: R,
+): Exclude<R, undefined | null>
+export function get<T, R>(
+  object: T,
+  accessorFn: AccessorFunction<T, R>,
+  defaultValue?: R,
+): R | undefined {
+  try {
+    const result = accessorFn(object)
+    return result === undefined || result === null ? defaultValue : result
+  } catch (e) {
+    return defaultValue
+  }
+}
+
+export default get
